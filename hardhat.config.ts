39a28d5bb2831a0f5dad58719fa8f4c2925856ba
VladyslavPerederWoof <vlad.p@woof.software>
2026-04-24 15:43:10 +0300

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,58 @@
+import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import { configVariable, defineConfig } from "hardhat/config";
+import hardhatMarkup from "@solarity/hardhat-markup";
+import hardhatContractSizer from "@solidstate/hardhat-contract-sizer";
+import hardhatAbiExporter from "@solidstate/hardhat-abi-exporter";
+
+export default defineConfig({
+    plugins: [hardhatMarkup, hardhatContractSizer, hardhatAbiExporter, hardhatToolboxMochaEthersPlugin],
+    solidity: {
+        profiles: {
+            default: {
+                version: "0.8.34",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200
+                    }
+                }
+            },
+            production: {
+                version: "0.8.34",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200
+                    }
+                }
+            }
+        }
+    },
+    networks: {
+        hardhatMainnet: {
+            type: "edr-simulated",
+            chainType: "l1"
+        },
+        hardhatOp: {
+            type: "edr-simulated",
+            chainType: "op"
+        },
+        sepolia: {
+            type: "http",
+            chainType: "l1",
+            url: configVariable("SEPOLIA_RPC_URL"),
+            accounts: [configVariable("SEPOLIA_PRIVATE_KEY")]
+        },
+        mainnet: {
+            type: "http",
+            chainType: "l1",
+            url: configVariable("MAINNET_RPC_URL"),
+            accounts: [configVariable("MAINNET_PRIVATE_KEY")]
+        }
+    },
+    verify: {
+        etherscan: {
+            apiKey: configVariable("ETHERSCAN_API_KEY")
+        }
+    }
+});
